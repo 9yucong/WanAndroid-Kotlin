72e73af29108d8bd9ba636f5ff9db3e368e3f1ae
Cong.Gao <cong.gao@welab-inc.com>
2022-06-29 13:52:48 +0800

    代码整理

--- app/src/main/java/com/cong/wanandroid/ui/home/HomeFragment.kt
+++ app/src/main/java/com/cong/wanandroid/ui/home/HomeFragment.kt
@@ -1,18 +1,14 @@
 package com.cong.wanandroid.ui.home
 
-import android.content.res.Resources
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.cong.wanandroid.R
 import com.cong.wanandroid.databinding.FragmentHomeBinding
 import com.cong.wanandroid.view.SpaceItemDecoration
 import kotlinx.coroutines.flow.collect
@@ -30,12 +26,12 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        val homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
         initView()
         lifecycleScope.launch {
-            homeViewModel.getPagingArticle().collect { pagingData->
+            homeViewModel.getPagingArticle().collect { pagingData ->
                 articleAdapter.submitData(pagingData)
             }
         }
@@ -44,11 +40,12 @@ class HomeFragment : Fragment() {
 
     private fun initView() {
         articleAdapter = ArticleAdapter()
-        val rvArticleList = binding.rvArticleList
-        rvArticleList.adapter = articleAdapter
-        rvArticleList.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
         val spaceItemDecoration = SpaceItemDecoration(16)
-        rvArticleList.addItemDecoration(spaceItemDecoration)
+        binding.rvArticleList.apply {
+            adapter = articleAdapter
+            layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+            addItemDecoration(spaceItemDecoration)
+        }
     }
 
     override fun onDestroyView() {
